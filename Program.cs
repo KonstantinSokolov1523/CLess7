66f2a5196df6b7fdcd51427dfc179a9f3573f44a
VIRTUOSO <kostyakot817@gmail.com>
2023-06-03 23:59:47 +0500

    all tasks solved

--- Program.cs
+++ Program.cs
@@ -1,4 +1,30 @@
-﻿/* Задача 47. Задайте двумерный массив размером m×n,
+﻿Console.Write("Введите номер задачи:");
+int task = Convert.ToInt32(Console.ReadLine());
+switch(task)
+{
+case 1:
+{
+    DoubleArray();
+    break;
+}
+case 2:
+{
+    FindNum();
+    break;
+}
+case 3:
+{
+    AverNumb();
+    break;
+}
+default:
+{
+    Console.WriteLine("некорректное значение");
+    break;
+}
+}
+
+/* Задача 47. Задайте двумерный массив размером m×n,
  заполненный случайными вещественными числами.
 
 m = 3, n = 4.
@@ -36,35 +62,46 @@ void DoubleArray()
 8 4 2 4
 
 17 -> такого числа в массиве нет */
+void FindNum()
+{
 Console.Write("Введите ваше число: ");
 int num = Convert.ToInt32(Console.ReadLine());
-int[] prs = new int[2];
-int ctr = prs[0];
-int stol = prs[1];
+bool pos = false;
 int[,] number = new int[3, 4];
 Randarr(number);
 PrintArr(number);
-FindNum(num);
-    
-if(ctr >= 0 & stol >= 0)
+for (int i = 0; i < number.GetLength(0); i++)
 {
-    Console.WriteLine($"строка {stol + 1} столбец {ctr + 1}");    
-}
-else
-{
-    Console.WriteLine($"Числа {num} нет в массиве");
-}
-    
-    void Randarr(int[,] array)
+        if (!pos)
     {
-        for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < number.GetLength(1); j++)
         {
-            for (int j = 0; j < array.GetLength(1); j++)
+            if (num == number[i, j])
             {
-                array[i, j] = new Random().Next(0, 10);
+                pos = true;
+                Console.WriteLine($"Столбец - {i + 1} строка - {j + 1}");
+                break;
             }
+            
         }
-    }  
+        
+    }
+}
+    if (!pos)
+    {
+        Console.WriteLine($"Числа {num} в массиве нет");
+    }
+
+void Randarr(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}  
 
 
     void PrintArr(int[,] array)
@@ -78,24 +115,8 @@ else
             Console.WriteLine("");
         }
     }
-    int[] FindNum(int num)
-    {
-        for(int i = 0; i < number.GetLength(1); i++)
-        {
-            for(int j = 0; j < number.GetLength(0); j++ )
-            {
-                if(number[j, i] == num)
-                { 
-                prs[0] = i;
-                prs[1] = j;
-                }
-            }
-        }
-    return prs;
-    }
 
-
-
+}
 /* Задача 52. Задайте двумерный массив из целых 
 чисел. Найдите среднее арифметическое элементов 
 в каждом столбце.
